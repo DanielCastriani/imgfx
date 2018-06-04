52075ed65d6db1a1210b78dbcb29a0d32b61383c
DanielCastriani <dcastriani@gmail.com>
2018-06-04 00:27:35 -0300

    adicionado: Rotaćão e alterado: modo de capturar nome

--- Efx.py
+++ Efx.py
@@ -1,3 +1,4 @@
+# pylint: disable=E1101
 import cv2
 import numpy as np
 
@@ -5,27 +6,35 @@ class Efx:
     def __init__(self,imgSrc,name):
         self.imgSrc = imgSrc
         self.name = name
-    
+
     def filpH(self,destPath,name):
         destImg = cv2.flip(self.imgSrc,0)
-        cv2.imwrite(destPath+"/"+ name+ "_FlipH.png",destImg)        
+        self.save(destImg,destPath,name,"FlipH")
 
-    def filpV(self,destPath,name):   
-        destImg = cv2.flip(self.imgSrc,1)             
-        cv2.imwrite(destPath+"/"+ name+"_FlipV.png",destImg)
+    def filpV(self,destPath,name):
+        destImg = cv2.flip(self.imgSrc,1)
+        self.save(destImg,destPath,name,"FlipV")
 
     def grayScale(self,destPath,name):
-        destImg = cv2.cvtColor(self.imgSrc,cv2.COLOR_BGR2GRAY)        
-        cv2.imwrite(destPath+"/"+ name+ "_Grayscale.png",destImg)
-    
+        destImg = cv2.cvtColor(self.imgSrc,cv2.COLOR_BGR2GRAY)
+        self.save(destImg,destPath,name,"Grayscale")
+
     def blur(self,destPath,name):
-        destImg = cv2.blur(self.imgSrc,(5,5))      
-        cv2.imwrite(destPath+"/"+ name+ "_blur.png",destImg)
+        destImg = cv2.blur(self.imgSrc,(5,5))
+        self.save(destImg,destPath,name,"blur")
 
     def filter_brightnes_contrast(self,destPath,name,brightness,contrast):
         """g(x) = contrast*f(x) + brightness"""
         hsv = cv2.addWeighted(self.imgSrc,contrast,np.zeros(self.imgSrc.shape,self.imgSrc.dtype),0,brightness)
-        t = " [Brightness,Contrast] [" + str(brightness)+ "," + str(contrast) + "]"
-        cv2.imwrite(destPath+"/" + name + " " + t + "_.png",hsv)
+        t = "[Brightness,Contrast] [" + str(brightness)+ "," + str(contrast) + "]"
+        # cv2.imwrite(destPath+"/" + name + t + ".png",hsv)
+        self.save(hsv,destPath,name,t)
 
-    
+    def rotate(self,destPath,name,angle):
+        rows,cols,ch = self.imgSrc.shape
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
+        destImg = cv2.warpAffine(self.imgSrc,M,(cols,rows))
+        self.save(destImg,destPath,name,"rotate[" + str(angle) +"]")
+
+    def save(self,img,dest,name,opr):
+        cv2.imwrite(dest+"/"+ name + " _" + opr + ".png",img) 
--- mainWindowController.py
+++ mainWindowController.py
@@ -1,4 +1,4 @@
-
+# pylint: disable=E1101
 from PyQt5 import QtCore, QtGui, QtWidgets
 from threading import Thread
 from Efx import Efx
@@ -8,7 +8,7 @@ import os
 import imghdr
 import cv2
 import re
-import sys
+import sys,time
 import numpy as np
 
 class MainWindowController(object):
@@ -20,12 +20,12 @@ class MainWindowController(object):
         self.pathQueue = []
         self.__setup()
         self.init_frm()
-        
+
     def __setup(self):
         self.ui.pbApply.clicked.connect(self.onClicked_Apply)
         self.ui.pbOpenSrcFolder.clicked.connect(self.onClicked_SrcPath)
         self.ui.pbOpenDestFolder.clicked.connect(self.onClicked_DestPath)
-   
+
     def openFolder(self):
         dialog = QtWidgets.QFileDialog()
         dialog.setFileMode(QtWidgets.QFileDialog.FileMode())
@@ -45,9 +45,9 @@ class MainWindowController(object):
         srcPath = self.ui.leSrcPath.text()
         destPath = self.ui.leDestPath.text()
         if len(srcPath) > 0 and len(destPath) > 0:
-            self.ui.pbApply.setEnabled(True)        
+            self.ui.pbApply.setEnabled(True)
         else:
-            self.ui.pbApply.setEnabled(False) 
+            self.ui.pbApply.setEnabled(False)
 
     def onClicked_SrcPath(self):
         path = self.openFolder()
@@ -61,8 +61,8 @@ class MainWindowController(object):
                     isImg = imghdr.what(x)
                     if isImg:
                         paths += x + '\n'
-                        queue.heappush(self.pathQueue,x)                       
-                    else: 
+                        queue.heappush(self.pathQueue,x)
+                    else:
                         print('Not an image')
         self.ui.plainTextEdit.setPlainText(paths)
 
@@ -70,7 +70,7 @@ class MainWindowController(object):
         self.ui.leDestPath.setText(self.openFolder())
         self.enableBtnApply()
 
-    def onClicked_Apply(self):        
+    def onClicked_Apply(self):
         self.ui.pbStatus.setMaximum(len(self.pathQueue))
         qtd = 0
         dest = self.ui.leDestPath.text().replace("\\","/")
@@ -78,13 +78,18 @@ class MainWindowController(object):
             path = queue.heappop(self.pathQueue)
             path = path.replace("\\","/")
             srcImg = cv2.imread(path)
-        
             if(srcImg is None):
                 print("Imagem nula:",path)
             else:
-                name = re.split("\\\\|\/|\.",path)[-2]
+                #name = re.split("\\\\|\/|\.",path)[-2]               
+
+                aux_name = re.split('\/',path)[-1]
+                name = ''
+                for i in range(len(aux_name) - 4):
+                    name += aux_name[i]
+
                 efx = Efx(srcImg,name)
-                
+
                 if self.ui.cbFlipH.isChecked():
                     efx.filpH(dest,name)
                 if self.ui.cbFlipV.isChecked():
@@ -93,16 +98,31 @@ class MainWindowController(object):
                     efx.blur(dest,name)
                 if self.ui.cbGrayScale.isChecked():
                     efx.grayScale(dest,name)
+
                 if self.ui.cbBrightness.isChecked():
                     efx.filter_brightnes_contrast(dest,name,30,1)
                     efx.filter_brightnes_contrast(dest,name,-30,1)
                     efx.filter_brightnes_contrast(dest,name,50,1)
                     efx.filter_brightnes_contrast(dest,name,-50,1)
+                    efx.filter_brightnes_contrast(dest,name,80,1)
+                    efx.filter_brightnes_contrast(dest,name,-80,1)
+
                 if self.ui.cbContrast.isChecked():
-                    efx.filter_brightnes_contrast(dest,name,0,0.8)
-                    efx.filter_brightnes_contrast(dest,name,0,1.2)                
+                    efx.filter_brightnes_contrast(dest,name,0,0.6)
                     efx.filter_brightnes_contrast(dest,name,0,0.7)
+                    efx.filter_brightnes_contrast(dest,name,0,0.8)
+                    efx.filter_brightnes_contrast(dest,name,0,1.2)
                     efx.filter_brightnes_contrast(dest,name,0,1.3)
+                    efx.filter_brightnes_contrast(dest,name,0,1.4)
+
+                if self.ui.cbRotationAnt.isChecked():
+                    efx.rotate(dest,name,-10)
+                    efx.rotate(dest,name,-15)
+
+                if self.ui.cbRotationAnt.isChecked():
+                    efx.rotate(dest,name,10)
+                    efx.rotate(dest,name,15)
+
             qtd+=1
             self.ui.pbStatus.setValue(qtd)
         self.ui.plainTextEdit.setPlainText("Finished")
@@ -111,4 +131,3 @@ if __name__ == "__main__":
     ctr = MainWindowController()
     ctr.MainWindow.show()
     sys.exit(ctr.app.exec_())
-    
